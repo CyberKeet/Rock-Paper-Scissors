3f22b8065c467c7d5ef0ab22efea5e496afc4b84
Katie <katherine.straughn@gmail.com>
2017-06-03 13:32:30 -0500

    Add files via upload

---
+++ EpicRPSDemo/EpicRPSDemo/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace EpicRPSDemo
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.instructionsLabel = new System.Windows.Forms.Label();
+            this.rockButton = new System.Windows.Forms.Button();
+            this.scissorsButton = new System.Windows.Forms.Button();
+            this.paperButton = new System.Windows.Forms.Button();
+            this.winLosePanel = new System.Windows.Forms.Panel();
+            this.playAgainButton = new System.Windows.Forms.Button();
+            this.winLoseLabel = new System.Windows.Forms.Label();
+            this.compItemPic = new System.Windows.Forms.PictureBox();
+            this.userItemPic = new System.Windows.Forms.PictureBox();
+            this.userScorePic = new System.Windows.Forms.PictureBox();
+            this.compScorePic = new System.Windows.Forms.PictureBox();
+            this.winLosePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.compItemPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userItemPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userScorePic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.compScorePic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // instructionsLabel
+            // 
+            this.instructionsLabel.AutoSize = true;
+            this.instructionsLabel.Location = new System.Drawing.Point(237, 300);
+            this.instructionsLabel.Name = "instructionsLabel";
+            this.instructionsLabel.Size = new System.Drawing.Size(110, 13);
+            this.instructionsLabel.TabIndex = 2;
+            this.instructionsLabel.Text = "Please select an item:";
+            // 
+            // rockButton
+            // 
+            this.rockButton.Location = new System.Drawing.Point(94, 356);
+            this.rockButton.Name = "rockButton";
+            this.rockButton.Size = new System.Drawing.Size(75, 23);
+            this.rockButton.TabIndex = 3;
+            this.rockButton.Text = "Rock";
+            this.rockButton.UseVisualStyleBackColor = true;
+            this.rockButton.Click += new System.EventHandler(this.rockButton_Click);
+            // 
+            // scissorsButton
+            // 
+            this.scissorsButton.Location = new System.Drawing.Point(248, 356);
+            this.scissorsButton.Name = "scissorsButton";
+            this.scissorsButton.Size = new System.Drawing.Size(75, 23);
+            this.scissorsButton.TabIndex = 4;
+            this.scissorsButton.Text = "Scissors";
+            this.scissorsButton.UseVisualStyleBackColor = true;
+            this.scissorsButton.Click += new System.EventHandler(this.scissorsButton_Click);
+            // 
+            // paperButton
+            // 
+            this.paperButton.Location = new System.Drawing.Point(408, 356);
+            this.paperButton.Name = "paperButton";
+            this.paperButton.Size = new System.Drawing.Size(75, 23);
+            this.paperButton.TabIndex = 5;
+            this.paperButton.Text = "Paper";
+            this.paperButton.UseVisualStyleBackColor = true;
+            this.paperButton.Click += new System.EventHandler(this.paperButton_Click);
+            // 
+            // winLosePanel
+            // 
+            this.winLosePanel.BackColor = System.Drawing.Color.White;
+            this.winLosePanel.Controls.Add(this.playAgainButton);
+            this.winLosePanel.Controls.Add(this.winLoseLabel);
+            this.winLosePanel.Location = new System.Drawing.Point(84, 284);
+            this.winLosePanel.Name = "winLosePanel";
+            this.winLosePanel.Size = new System.Drawing.Size(400, 150);
+            this.winLosePanel.TabIndex = 6;
+            this.winLosePanel.Visible = false;
+            // 
+            // playAgainButton
+            // 
+            this.playAgainButton.Location = new System.Drawing.Point(164, 72);
+            this.playAgainButton.Name = "playAgainButton";
+            this.playAgainButton.Size = new System.Drawing.Size(75, 23);
+            this.playAgainButton.TabIndex = 1;
+            this.playAgainButton.Text = "Play Again?";
+            this.playAgainButton.UseVisualStyleBackColor = true;
+            this.playAgainButton.Click += new System.EventHandler(this.playAgainButton_Click);
+            // 
+            // winLoseLabel
+            // 
+            this.winLoseLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.winLoseLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winLoseLabel.Location = new System.Drawing.Point(0, 0);
+            this.winLoseLabel.Margin = new System.Windows.Forms.Padding(30, 0, 3, 0);
+            this.winLoseLabel.Name = "winLoseLabel";
+            this.winLoseLabel.Size = new System.Drawing.Size(400, 69);
+            this.winLoseLabel.TabIndex = 0;
+            this.winLoseLabel.Text = "n/a";
+            this.winLoseLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.winLoseLabel.Click += new System.EventHandler(this.winLoseLabel_Click);
+            // 
+            // compItemPic
+            // 
+            this.compItemPic.Location = new System.Drawing.Point(372, 74);
+            this.compItemPic.Name = "compItemPic";
+            this.compItemPic.Size = new System.Drawing.Size(200, 200);
+            this.compItemPic.TabIndex = 1;
+            this.compItemPic.TabStop = false;
+            // 
+            // userItemPic
+            // 
+            this.userItemPic.Location = new System.Drawing.Point(12, 74);
+            this.userItemPic.Name = "userItemPic";
+            this.userItemPic.Size = new System.Drawing.Size(200, 200);
+            this.userItemPic.TabIndex = 0;
+            this.userItemPic.TabStop = false;
+            // 
+            // userScorePic
+            // 
+            this.userScorePic.Location = new System.Drawing.Point(12, 12);
+            this.userScorePic.Name = "userScorePic";
+            this.userScorePic.Size = new System.Drawing.Size(200, 50);
+            this.userScorePic.TabIndex = 7;
+            this.userScorePic.TabStop = false;
+            // 
+            // compScorePic
+            // 
+            this.compScorePic.Location = new System.Drawing.Point(372, 12);
+            this.compScorePic.Name = "compScorePic";
+            this.compScorePic.Size = new System.Drawing.Size(200, 50);
+            this.compScorePic.TabIndex = 8;
+            this.compScorePic.TabStop = false;
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 462);
+            this.Controls.Add(this.winLosePanel);
+            this.Controls.Add(this.compScorePic);
+            this.Controls.Add(this.userScorePic);
+            this.Controls.Add(this.paperButton);
+            this.Controls.Add(this.scissorsButton);
+            this.Controls.Add(this.rockButton);
+            this.Controls.Add(this.instructionsLabel);
+            this.Controls.Add(this.compItemPic);
+            this.Controls.Add(this.userItemPic);
+            this.Name = "GameForm";
+            this.Text = "Epic RPS";
+            this.winLosePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.compItemPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userItemPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userScorePic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.compScorePic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox userItemPic;
+        private System.Windows.Forms.PictureBox compItemPic;
+        private System.Windows.Forms.Label instructionsLabel;
+        private System.Windows.Forms.Button rockButton;
+        private System.Windows.Forms.Button scissorsButton;
+        private System.Windows.Forms.Button paperButton;
+        private System.Windows.Forms.Panel winLosePanel;
+        private System.Windows.Forms.Label winLoseLabel;
+        private System.Windows.Forms.Button playAgainButton;
+        private System.Windows.Forms.PictureBox userScorePic;
+        private System.Windows.Forms.PictureBox compScorePic;
+    }
+}
+
---
+++ EpicRPSDemo/EpicRPSDemo/Form1.cs
@@ -0,0 +1,190 @@
+﻿// AUthor: Katie Straughn
+// Date: 11/26/2016
+// Program: Epis RPS
+// Purpose: A classic game of rock-paper-scissors against the computer.
+// The first to win three matches wins the game.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace EpicRPSDemo
+{
+    public partial class GameForm : Form
+    {
+        // Declare and initialize scores for user and comp, and declare vars for their items
+        int userScore = 0, compScore = 0, userItem, compItem;
+
+        // Enumeration of the items
+        enum Items
+        {
+            ROCK = 1, PAPER, SCISSORS
+        }
+
+        // This enumeration holds a value stating who won
+        /* Not used currently */
+        enum Winner
+        {
+            USERWON, COMPWON, TIE
+        }
+
+        public GameForm()
+        {
+            InitializeComponent();
+            //SoundPlayer bgMusic = new SoundPlayer(@"C:\Users\Katie\Music\Katie's Songs\Town Theme 1.wav");
+            //bgMusic.Play();
+        }
+
+        /* This method activates when the rock button is clicked */
+        private void rockButton_Click(object sender, EventArgs e)
+        { 
+            userItem = (int)Items.ROCK;
+            ChangeUserImage();
+            ComputerSelectsItem();
+            FindWinner();
+        }
+
+        /* This method activates when the paper button is clicked */
+        private void paperButton_Click(object sender, EventArgs e)
+        {
+            userItem = (int)Items.PAPER;
+            ChangeUserImage();
+            ComputerSelectsItem();
+            FindWinner();
+        }
+
+        /* This method activates when the scissors button is clicked */
+        private void scissorsButton_Click(object sender, EventArgs e)
+        {
+            userItem = (int)Items.SCISSORS;
+            ChangeUserImage();
+            ComputerSelectsItem();
+            FindWinner();
+        }
+
+        /* This method activates when the play again button is clicked */
+        private void playAgainButton_Click(object sender, EventArgs e)
+        {
+            winLosePanel.Visible = false; // Hide panel                           
+            // Hide picture boxes
+            userItemPic.Image = null;
+            compItemPic.Image = null;
+            // Check if someone has won
+            if (userScore == 3 || compScore == 3)
+            {
+                // Reset scores
+                userScore = 0;
+                compScore = 0;
+                userScorePic.Image = null;
+                compScorePic.Image = null;
+            }
+        }
+
+        private void winLoseLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /* This method changes the user's item picture box image based on the item selected */
+        private void ChangeUserImage()
+        {
+            switch ((Items)userItem)
+            {
+                case Items.ROCK:
+                    userItemPic.Image = EpicRPSDemo.Properties.Resources.rock;
+                    break;
+                case Items.PAPER:
+                    userItemPic.Image = EpicRPSDemo.Properties.Resources.paper;
+                    break;
+                case Items.SCISSORS:
+                    userItemPic.Image = EpicRPSDemo.Properties.Resources.scissors;
+                    break;
+            }
+        }
+
+       /* This method randomizes an item for the computer, and changes it's picture box to match */
+       private void ComputerSelectsItem()
+       {
+           // Computer chooses an item at random
+           Random random = new Random();
+           int randomNumber = random.Next(1, 4);
+           compItem = (int)(Items)randomNumber;
+
+           // This switch statement changes compPic's image to match item selected
+           switch ((Items)compItem)
+            {
+                case Items.ROCK:
+                    compItemPic.Image = EpicRPSDemo.Properties.Resources.rock;
+                    break;
+                case Items.PAPER:
+                    compItemPic.Image = EpicRPSDemo.Properties.Resources.paper;
+                    break;
+                case Items.SCISSORS:
+                    compItemPic.Image = EpicRPSDemo.Properties.Resources.scissors;
+                    break;
+            }
+        }
+
+       /* This method finds the winner of the match */
+       private void FindWinner()
+       {
+            int theWinner; // Hold the enumeration Winner value to find who earns a point
+
+           // If statments compare the user's chosen item and the computer's to determine the winner
+           // Both the user and comp having the same item will result in a tie
+           if ((userItem == (int)Items.ROCK && compItem == (int)Items.ROCK) ||
+                (userItem == (int)Items.PAPER && compItem == (int)Items.PAPER) ||
+                (userItem == (int)Items.SCISSORS && compItem == (int)Items.SCISSORS))
+            {
+                winLoseLabel.Text = "It's a tie!"; // label will display "it's a tie!"
+                //theWinner = (int)Winner.TIE;
+            }
+           // If the user has chosen the stronger item, then they win
+           else if ((userItem == (int)Items.ROCK && compItem == (int)Items.SCISSORS) ||
+                (userItem == (int)Items.PAPER && compItem == (int)Items.ROCK) ||
+                (userItem == (int)Items.SCISSORS && compItem == (int)Items.PAPER))
+            {
+                winLoseLabel.Text = "You win!"; // label will display "you win!"
+                userScore += 1;
+                //theWinner = (int)Winner.USERWON;
+            }
+            else // Any other aterantive will result in a lose
+            {
+                winLoseLabel.Text = "You lose..."; // label will display "you lose..."
+                compScore += 1;
+                //theWinner = (int)Winner.COMPWON;
+            }
+
+            winLosePanel.Visible = true; // panel made visible
+
+            // Increment the score of the user if they won
+            if (userScore == 1)
+                userScorePic.Image = EpicRPSDemo.Properties.Resources.point1;
+            else if (userScore == 2)
+                userScorePic.Image = EpicRPSDemo.Properties.Resources.point2;
+            else if (userScore == 3) // If the score has reached 3, you have won
+            {
+                userScorePic.Image = EpicRPSDemo.Properties.Resources.point3;
+                winLoseLabel.Text = "You have won the game!";
+            }
+
+            // Increment the score of the comp if it won
+            if (compScore == 1)
+                compScorePic.Image = EpicRPSDemo.Properties.Resources.point1;
+            else if (compScore == 2)
+                compScorePic.Image = EpicRPSDemo.Properties.Resources.point2;
+            else if (compScore == 3) // If the score has reached 3, the computer has won
+            {
+                compScorePic.Image = EpicRPSDemo.Properties.Resources.point3;
+                winLoseLabel.Text = "The compter has won the game!";
+            }
+        }
+    }
+}
